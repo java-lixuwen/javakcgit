5ed934083716d19ec6d775f5590f3ae07c13999e
javakc_global <javakc_global@javakc.com>
2021-01-05 11:51:44 +0800

    测试冲突v2.0

--- src/main/java/com/javakc/TestGit.java
+++ src/main/java/com/javakc/TestGit.java
@@ -7,7 +7,7 @@ package com.javakc;
  * @create: 2021-01-04 23:18
  **/
 public class TestGit {
-    private String name;
+    private String name_javakc83;
     private String password;
     private int age;
 }
